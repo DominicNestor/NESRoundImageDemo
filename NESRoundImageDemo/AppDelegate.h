3fa5ab51151348900379644bd669964e0ecad577
Nestor <Nestor@Nestors-MacBook.local>
2015-01-28 10:07:25 +0800

    Initial Commit

---
+++ NESRoundImageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NESRoundImageDemo
+//
+//  Created by Nestor on 15/1/28.
+//  Copyright (c) 2015年 NesTalk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
